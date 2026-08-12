d04e437d9aaeb1a2ce68c65e69d61e89f062b4d4
Kruschka <rebecca.kruschka@web.de>
2026-08-12 06:05:52 +0200

    Development/Test_set_split

--- Iterative_Multilabel_Stratification_Test_Development_Split.py
+++ Iterative_Multilabel_Stratification_Test_Development_Split.py
@@ -4,13 +4,13 @@ import pandas as pd
 from iterstrat.ml_stratifiers import MultilabelStratifiedShuffleSplit
 
 # Loading the dataset with the 8 labels
-df = pd.read_excel("C:\Users\rebec\OneDrive\Dokumente\UT 25_26\Master_Thesis\Reddit_modeling_dataset_8Labels.xlsx")
+df = pd.read_excel("Reddit_modeling_dataset_8Labels.xlsx")
 
 # Initiating X as the retrieved Reddit-posts
 X = df["Text"]
 
-# Initiating y as the 8 binary label columns
-y = df[
+# Define label_columns as the 8 binary label columns
+label_columns = [
         "Death Anxiety",
         "Death Acceptance",
         "Loneliness",
@@ -21,18 +21,42 @@ y = df[
         "Engagement"
 ]
 
+# Creating the multilabel matrix y consisting of 8 label columns, all posts and and 0/1 as coded values per post
 y = df[label_columns].to_numpy()
 
 
+# Implementing the 90/10 development-test-set split with a single split (dev/test), 
+# an assignment of 10% posts to the test set, 
+# and a fixed random seed for reproduction purposes
+sss = MultilabelStratifiedShuffleSplit(
+    n_splits=1, 
+    test_size=0.10, 
+    random_state=42
+)
 
-sss = StratifiedShuffleSplit(n_splits=1, test_size=0.10, random_state=0)
-sss.get_n_splits()
+# Indices for dev and test sets are generated
+dev_index, test_index = next(sss.split(X, y))
 
-print(sss)
+# Retrieving the datasets
+dev_set = df.iloc[dev_index].copy() 
+test_set = df.iloc[test_index].copy()
+
+# Checking the sets' number of included posts
+print("Development set: ", len(dev_set))
+print("Test set: ", len(test_set))
+
+# Print the number of positive instances per label
+print("\nFull Dataset positive instances per label: ")
+print(df[label_columns].sum())
+
+print("\nDevelopment set positive instances per label:")
+print(dev_set[label_columns].sum())
+
+print("\nTest set positive instances per label:")
+print(test_set[label_columns].sum())
 
 
+# Save the newly created Development set and Test set as csv files
+dev_set.to_csv("Development_set.csv", index = False)
+test_set.to_csv("Test_set.csv", index = False)
 
-class sklearn.model_selection.StratifiedShuffleSplit()
-
-    for i, train_index, test_index) in enumerate(sss.split(X, y))
-        print 
